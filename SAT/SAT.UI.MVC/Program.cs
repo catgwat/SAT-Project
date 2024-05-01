0b809ece416c8195243cd2fc82efb5a15eb3ddf4
HossinatorWCheez <ethanjames.aa@outlook.com>
2024-05-01 09:58:10 -0500

    Structured _Layout in the UI layer

--- SAT/SAT.UI.MVC/Program.cs
+++ SAT/SAT.UI.MVC/Program.cs
@@ -14,6 +14,7 @@ namespace SAT.UI.MVC
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
+
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
